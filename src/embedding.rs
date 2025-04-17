15043d864600969cab38755872982c370a16ea32
ViniciusTheCoder <vinigurskiferraz@gmail.com>
2025-04-16 22:51:53 -0300

    feat: improving embeddings creation

--- src/embedding.rs
+++ src/embedding.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::ops::{Add, Mul};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Embedding(pub Vec<f32>);
@@ -38,5 +39,57 @@ impl Embedding {
         } else {
             0.0
         }
+    }
+    
+    pub fn distance(&self, other: &Embedding) -> f32 {
+        if self.dimension() != other.dimension() {
+            panic!("Different dimensions");
+        }
+        
+        self.0.iter()
+            .zip(other.0.iter())
+            .map(|(&a, &b)| (a - b).powi(2))
+            .sum::<f32>()
+            .sqrt()
+    }
+    
+    pub fn top_k_similarity(&self, others: &[Embedding], k: usize) -> Vec<(usize, f32)> {
+        let mut similarities: Vec<(usize, f32)> = others.iter()
+            .enumerate()
+            .map(|(i, other)| (i, self.cosine_similarity(other)))
+            .collect();
+            
+        similarities.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap_or(std::cmp::Ordering::Equal));
+        similarities.truncate(k);
+        similarities
+    }
+}
+
+impl Add for &Embedding {
+    type Output = Embedding;
+    
+    fn add(self, other: &Embedding) -> Embedding {
+        if self.dimension() != other.dimension() {
+            panic!("Cannot add embeddings of different dimensions");
+        }
+        
+        let values = self.0.iter()
+            .zip(other.0.iter())
+            .map(|(&a, &b)| a + b)
+            .collect();
+            
+        Embedding(values)
+    }
+}
+
+impl Mul<f32> for &Embedding {
+    type Output = Embedding;
+    
+    fn mul(self, scalar: f32) -> Embedding {
+        let values = self.0.iter()
+            .map(|&v| v * scalar)
+            .collect();
+            
+        Embedding(values)
     }
 }
